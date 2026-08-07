c6d75c255058c14a26efbf8ea0ff34101c788fc8
kongeebowl <kadenpang09@gmail.com>
2026-08-07 10:35:13 -0400

    i hate interfaces and methods holy crap

--- slop/hashmap.h
+++ slop/hashmap.h
@@ -1,4 +1,23 @@
 #ifndef HASHMAP_H
 #define HASHMAP_H
 
-#endif // !DEBUG
+// define hashtable struct
+typedef struct ht ht;
+// create function
+ht* ht_create(void);
+// destroy function
+void ht_destroy(ht* table);
+// get function
+void* ht_get(ht* table, const char* key)
+// set function, sets the value of a key
+
+// length function
+
+// struct for the hashtable iterator
+
+// return fuction with the hashtable iterator
+
+// move iterator to the next item in the hashtable, update the iterator's key
+// and value to current item and return true, no items = false
+
+#endif
