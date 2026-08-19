aae514ce64e6ce587b2f1d3636267d9fa9c4c84f
kongeebowl <kadenpang09@gmail.com>
2026-08-19 09:03:31 -0400

    die

--- calc/main.c
+++ calc/main.c
@@ -1,23 +1,10 @@
 #include <curses.h>
 
-void draw_calculator(double result, double current_num, char op) {
+void draw_calculator(double result, double current_num) {
   clear();
-  mvprintw(1, 2, "=========================");
-  mvprintw(2, 2, "|    NCURSES CALC      |");
-  mvprintw(3, 2, "=========================");
 
-  mvprintw(5, 2, " [ Active Op: %c ]", op ? op : ' ');
-  mvprintw(6, 2, " [ Current ]: %.4g", current_num);
-  mvprintw(7, 2, " [ Result  ]: %.4g", result);
-  mvprintw(8, 2, "=========================");
-
-  mvprintw(10, 2, "Controls:");
-  mvprintw(11, 2, "  0-9 : Enter digits");
-  mvprintw(12, 2, "  + - * / : Operations");
-  mvprintw(13, 2, "  Enter   : Calculate (=)");
-  mvprintw(14, 2, "  C       : Clear display");
-  mvprintw(15, 2, "  Q       : Quit application");
-  mvprintw(17, 2, "Enter Input: ");
+  mvprintw(4, 2, "Current Number: %f", current_num);
+  mvprintw(6, 2, "Result: %f", result);
 
   refresh();
 }
@@ -32,10 +19,11 @@ int main(void) {
   double result = 0.0;
   double current_num = 0.0;
   char current_op = 0.0;
-  int input;
+  int input = 0;
 
   while (1) {
-    draw_calculator(result, current_num, current_op);
+    draw_calculator(result, current_num);
+    // getnstr(&stuff, sizeof(input) - 1);
     input = getch();
 
     if (input == 'q' || input == 'Q') {
@@ -46,6 +34,7 @@ int main(void) {
       result = 0.0;
       current_num = 0.0;
       current_op = 0;
+      // stuff = '\0';
       continue;
     }
 
@@ -80,8 +69,8 @@ int main(void) {
           result /= current_num;
       }
       current_num = result;
-      current_op = 0;
-      mvprintw(19, 2, "Result: %f", result);
+      current_op = 1;
+      refresh();
     }
   }
 
